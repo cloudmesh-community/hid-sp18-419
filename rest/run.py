14926246268d350d05040f914590b71749727c5c
bsobolik <bsobolik@gmail.com>
2018-02-05 20:46:53 -0500

    fixes to run.py

--- rest/run.py
+++ rest/run.py
@@ -8,21 +8,26 @@ from flask import Response
 
 app = Eve()
 
+def get_response(data):
+    sdata = json.dumps(data.__dict__)
+
+    response = Response()
+    response.headers["status"] = 200
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    response.data = sdata
+
+    return response
+    
+
 @app.route('/disk', methods=['GET'])
 def get_disk_info():    
     du = psutil.disk_usage('/')
     pct_str = str(du.percent) + '%'
     disk = Disk(platform.platform(), size(du.total), size(du.used),
                     size(du.free), pct_str)
-
-    sdata = json.dumps(disk.__dict__)
-
-    response = Response()
-    response.headers["status"] = 200
-    response.headers["Content-Type"] = "application/json; charset=utf-8"
-    response.data = sdata
     
-    return response
+    return get_response(disk)
+
 
 if __name__ == "__main__":
     app.run()
