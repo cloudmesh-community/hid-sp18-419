339d056f659616d76978c79c60abab84334a6c66
bsobolik <bsobolik@gmail.com>
2018-02-13 21:40:00 -0500

    simplified swagger assignment

--- swagger/gce_vm_instances/server/gce/flaskConnexion/swagger_server/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.disk import DISK
-from swagger_server.models.vm import VM
-from swagger_server.models.vm_tags import VMTags
