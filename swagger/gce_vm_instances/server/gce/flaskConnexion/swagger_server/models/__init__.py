f6ebaef01f57d608bc59be1dbbf78067da207402
bsobolik <bsobolik@gmail.com>
2018-02-16 10:45:10 -0500

    regenerated code without byte format

---
+++ swagger/gce_vm_instances/server/gce/flaskConnexion/swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.vm import VM
+from swagger_server.models.vm_tags import VMTags
