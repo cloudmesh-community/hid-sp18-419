e85bad04726038596eec82e905a6cd5f5a1ae40a
bsobolik <bsobolik@gmail.com>
2018-02-25 17:09:36 -0500

    Implemented vms functions

--- swagger/gce_vm_instances/server/gce/flaskConnexion/swagger_server/controllers/default_controller.py
+++ swagger/gce_vm_instances/server/gce/flaskConnexion/swagger_server/controllers/default_controller.py
@@ -1,9 +1,14 @@
 import connexion
 import six
 
+import googleapiclient.discovery
+import json
+
 from swagger_server.models.vm import VM  # noqa: E501
 from swagger_server import util
 
+GCP_PROJECT = "nth-subset-194702"
+GCP_ZONE = "us-east1-b"
 
 def vms_get():  # noqa: E501
     """vms_get
@@ -13,7 +18,22 @@ def vms_get():  # noqa: E501
 
     :rtype: List[VM]
     """
-    return 'do some magic!'
+    vms = []
+    compute = googleapiclient.discovery.build('compute', 'v1')
+    results = compute.instances().list(project=GCP_PROJECT,
+                                       zone=GCP_ZONE).execute()
+    for result in results['items']:
+        vm = VM(id=result['id'],
+                creation_timestamp=result['creationTimestamp'],
+                name=result['name'],
+                description=result['description'],
+                machine_type=result['machineType'],
+                status=result['status'],
+                zone=result['zone'],
+                can_ip_forward=result['canIpForward'])
+        vms.append(vm)
+    
+    return vms
 
 
 def vms_id_get(id):  # noqa: E501
@@ -26,4 +46,7 @@ def vms_id_get(id):  # noqa: E501
 
     :rtype: VM
     """
-    return 'do some magic!'
+    vms = vms_get()
+    vm = vms['id'==id]
+    
+    return vm
