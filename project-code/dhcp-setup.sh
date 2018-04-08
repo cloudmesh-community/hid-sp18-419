5a2717580f5c2ead3c6c7a93f626b63c2574a7ba
bsobolik <bsobolik@gmail.com>
2018-04-07 21:56:53 -0400

    first cut at startup script

---
+++ project-code/dhcp-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install isc-dhcp-server
+
+cat <<EOT >> /etc/default/isc-dhcp-server
+subnet 192.168.2.0 netmask 255.255.255.0 {
+    range 192.168.2.100 192.168.2.200;
+    option broadcast-address 192.168.2.255;
+    option routers 192.168.2.1;
+    max-lease-time 7200;
+    option domain-name "red00";
+    option domain-name-servers 8.8.8.8;
+INTERFACES=""
+iface eth0 inet dhcp
+}
+EOT
+
+sed -i -e 's/INTERFACES=\"\"/INTERFACES=\"eth0\"/g' /etc/default/isc-dhcp-server
+
+sed -i -e 's/iface eth0 inet dhcp/\# iface eth0 inet dhcp/g' /etc/network/interfaces
+
+cat <<EOT >> /etc/network/interfaces
+auto eth0
+iface eth0 inet static
+    address 192.168.2.1
+    netmask 255.255.255.0
+}
+EOT
+
+sudo service isc-dhcp-server stop
+sudo service isc-dhcp-server start
+
+sudo update-rc.d -f dhcp-setup.sh remove
