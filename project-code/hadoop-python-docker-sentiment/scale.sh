867a87d99414d5d603553d2ef489760f8b58e764
Min Chen <min.chen.1015@gmail.com>
2018-04-17 22:37:51 -0400

    modify makefile to add scalability of number of workers by passing the argument directly

---
+++ project-code/hadoop-python-docker-sentiment/scale.sh
@@ -0,0 +1,19 @@
+if [ $# -ne 1 ]; then
+    echo "unknown number of workers, using 1 as default"
+    cp ./docker-compose-template.yml ./docker-compose.yml
+    exit 0
+fi
+
+cp ./docker-compose-template.yml ./docker-compose.yml
+for i in $(seq 2 $1)
+do
+printf "  worker"$i":\n\
+    build: ./hadoop-worker\n\
+    container_name: \"worker"$i"\"\n\
+    ports:\n\
+      - \"$((9900+$i)):9864\"\n\
+      - \"$((8040+$i)):8042\"\n\n"\
+>>./docker-compose.yml
+done
+echo "docker-compose.yml created with #worker "$1
+exit 0
