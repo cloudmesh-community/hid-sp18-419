7f403aa3cb479b22072ddd18bf897aebc7c67e7d
bsobolik <bsobolik@gmail.com>
2018-04-23 20:28:05 -0400

    added Makefile

--- project-code/vm-config/runall.sh
+++ project-code/vm-config/runall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # cd /cloudmesh/python
-time ./runPythonMapReduce.sh 2>&1 | tee -a /cloudmesh/python/log.txt
+time ./runPythonMapReduce.sh 2>&1 | tee -a log.txt
+
+hadoop fs -put log.txt .
