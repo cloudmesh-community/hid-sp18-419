1c7dab93a71800a1e25bf501b18906d250ba3b4f
bsobolik <bsobolik@gmail.com>
2018-04-26 15:35:59 -0400

    removing unnecessary files

--- project-code/pi-config/setup-pi.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Enable ssh
-echo -e "raspberry" | sudo systemctl enable ssh
-echo -e "raspberry" | sudo systemctl start ssh
-
-# Change password
-
-echo -e "raspberry\nsnowcluster\nsnowcluster" | passwd
